148843e12b91c44ed4b9634cb5f6ac25ffcff644
Renan C Vitor <renan.vitor.cm@gmail.com>
2024-04-15 20:04:00 -0300

    Tabuada do número 5.

--- estrutura-sequencial/src/Main.java
+++ estrutura-sequencial/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int num = 5;
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(num + "x" + i + " = " + (num * i));
+            //System.out.printf("%dx%d = %d%n", num, i, num * i);
+        }
     }
 }
