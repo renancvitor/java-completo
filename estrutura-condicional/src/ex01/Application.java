c23223a1b1f8923a62441617fd8286ee6fde421d
Renan C Vitor <renan.vitor.cm@gmail.com>
2024-04-16 07:02:10 -0300

    Conclusão ex01

--- estrutura-condicional/src/ex01/Application.java
+++ estrutura-condicional/src/ex01/Application.java
@@ -1,7 +1,26 @@
 package ex01;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Ex 01");
+
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        int num;
+
+        System.out.print("Digite um número: ");
+        num = sc.nextInt();
+
+        if (num >= 0) {
+            System.out.println("Não negativo!");
+        }
+        else {
+            System.out.println("Negativo!");
+        }
+
+        sc.close();
     }
 }
