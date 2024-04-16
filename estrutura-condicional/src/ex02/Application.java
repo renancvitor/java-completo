6e864717da63b949c5b0b526e686343180bcf3eb
Renan C Vitor <renan.vitor.cm@gmail.com>
2024-04-16 07:05:48 -0300

    Conclusão ex02

--- estrutura-condicional/src/ex02/Application.java
+++ estrutura-condicional/src/ex02/Application.java
@@ -1,7 +1,24 @@
 package ex02;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        
+
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        int valor;
+
+        System.out.print("Digite um valor:  ");
+        valor = sc.nextInt();
+
+        if (valor % 2 == 0) {
+            System.out.println("Par!");
+        }
+        else {
+            System.out.println("Ímpar!");
+        }
     }
 }
