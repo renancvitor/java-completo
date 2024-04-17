fa61e8d0b62a2c559c905be7d19295b7d14869ec
Renan C Vitor <renan.vitor.cm@gmail.com>
2024-04-16 21:53:51 -0300

    First commit

---
+++ estruturas-repetitivas/src/ex01/Application.java
@@ -0,0 +1,11 @@
+package ex01;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
