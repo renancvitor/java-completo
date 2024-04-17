c140f4bb5ea43863508b61ff59612b5872ba0b6d
Renan C Vitor <renan.vitor.cm@gmail.com>
2024-04-17 07:55:40 -0300

    Conclusão ex01

--- estruturas-repetitivas/src/ex01/Application.java
+++ estruturas-repetitivas/src/ex01/Application.java
@@ -6,6 +6,21 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        int senha;
+
+        System.out.print("Digite sua senha:  ");
+        senha = sc.nextInt();
+
+        while (senha != 2002) {
+            System.out.println("Senha inválida!");
+            System.out.println();
+            System.out.print("Digite novamente sua senha:  ");
+            senha = sc.nextInt();
+        }
+        System.out.println("Senha correta! Acesso liberado.");
+
+        sc.close();
     }
 }
