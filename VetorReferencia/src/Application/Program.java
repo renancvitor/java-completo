74ded5ce57339307abf04da58c440cc5164106e8
Renan C Vitor <renan.vitor.cm@gmail.com>
2024-04-22 20:12:20 -0300

    Cap10 Vectors with Class

---
+++ VetorReferencia/src/Application/Program.java
@@ -0,0 +1,35 @@
+package Application;
+
+import Entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        Product[] vect = new Product[n];
+
+        for (int i = 0; i < vect.length; i++) {
+            sc.nextLine();
+            String name = sc.nextLine();
+            double price = sc.nextDouble();
+            vect[i] = new Product(name, price);
+        }
+
+        double sum = 0.0;
+        for (int i = 0; i < vect.length; i++) {
+            sum += vect[i].getPrice();
+        }
+        double avg = sum / vect.length;
+
+        System.out.println();
+        System.out.printf("Average price: $ %.2f%n", avg);
+
+        sc.close();
+    }
+}
