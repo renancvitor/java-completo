9f9940ba2eaf5b32e6d430994bb24b9f4c66260e
Renan C Vitor <renan.vitor.cm@gmail.com>
2024-05-01 20:00:48 -0300

    Conclusion exemplo 1 Try Catch

--- TryCatch/src/Program/Application.java
+++ TryCatch/src/Program/Application.java
@@ -7,6 +7,24 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
 
+        method1();
+
+        System.out.println("End of program..");
+
+    }
+
+    public static void method1() {
+
+        System.out.println("***** METHOD 1 START *****");
+        method2();
+        System.out.println("***** METHOD 1 END *****");
+
+    }
+
+    public static void method2() {
+
+        System.out.println("***** METHOD 2 START *****");
+
         Locale.setDefault(Locale.US);
         Scanner scanner = new Scanner(System.in);
 
@@ -17,12 +35,14 @@ public class Application {
         }
         catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Invalid position!!");
+            e.printStackTrace();
+            scanner.next();
         }
         catch (InputMismatchException e) {
             System.out.println("Input error!!");
         }
 
-        System.out.println("End of program..");
+        System.out.println("***** METHOD 2 END *****");
 
         scanner.close();
     }
