d97ce81b8c9255be1cf4a88d19b76402cdd8f11d
Renan C Vitor <renan.vitor.cm@gmail.com>
2024-05-01 19:51:38 -0300

    Conclusion exemplo Try Catch basic

---
+++ TryCatch/src/Program/Application.java
@@ -0,0 +1,29 @@
+package Program;
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            String[] vect = scanner.nextLine().split("  ");
+            int position = scanner.nextInt();
+            System.out.println(vect[position]);
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid position!!");
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Input error!!");
+        }
+
+        System.out.println("End of program..");
+
+        scanner.close();R
+    }
+}
