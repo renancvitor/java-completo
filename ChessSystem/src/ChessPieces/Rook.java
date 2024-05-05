4604f65097a6d7c385fd2386a2cbc5137aec3142
Renan C Vitor <renan.vitor.cm@gmail.com>
2024-05-05 16:55:35 -0300

    Project Chess System - Placing pieces on the board

---
+++ ChessSystem/src/ChessPieces/Rook.java
@@ -0,0 +1,17 @@
+package ChessPieces;
+
+import BoardGame.Board;
+import Chess.ChessPiece;
+import Chess.Color;
+
+public class Rook extends ChessPiece {
+
+    public Rook(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public String toString() {
+        return "R";
+    }
+}
