6bdd4b521c964ccf1ec4a462467c27ecb6ba35fc
Renan C Vitor <renan.vitor.cm@gmail.com>
2024-05-06 18:23:53 -0300

    Project Chess System - Implementing possible moves of Rook

--- ChessSystem/src/Chess/ChessPiece.java
+++ ChessSystem/src/Chess/ChessPiece.java
@@ -2,6 +2,7 @@ package Chess;
 
 import BoardGame.Board;
 import BoardGame.Piece;
+import BoardGame.Position;
 
 public abstract class ChessPiece extends Piece {
 
@@ -15,4 +16,11 @@ public abstract class ChessPiece extends Piece {
     public Color getColor() {
         return color;
     }
+
+    protected boolean isThereOpponetPiece(Position position) {
+        ChessPiece p = (ChessPiece)getBoard().piece(position);
+        return p != null && p.getColor() != color;
+    }
+
+
 }
