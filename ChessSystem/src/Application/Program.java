0d03e8bd271a62324e8a64511fdf437ce8495fd2
Renan C Vitor <renan.vitor.cm@gmail.com>
2024-05-07 19:41:16 -0300

    Project Chess System - Special move - Promotion

--- ChessSystem/src/Application/Program.java
+++ ChessSystem/src/Application/Program.java
@@ -40,6 +40,12 @@ public class Program {
                 if (capturedPiece != null) {
                     captured.add(capturedPiece);
                 }
+
+                if (chessMatch.getPromoted() != null) {
+                    System.out.print("Enter piece for promotion (B/N/R/Q):  ");
+                    String type = scanner.nextLine();
+                    chessMatch.replacePromotedPiece(type);
+                }
             }
             catch (ChessException | InputMismatchException e) {
                 System.out.println(e.getMessage());
