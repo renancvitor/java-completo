634617c29fd4b2d08a97c1468ff3ba1f640877bc
Renan C Vitor <renan.vitor.cm@gmail.com>
2024-05-09 20:27:42 -0300

    Delimited Generics exemplo

---
+++ DelimitedGenerics/src/Application/Program.java
@@ -0,0 +1,43 @@
+package Application;
+
+import Entities.Product;
+import Service.CalculationService;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        List<Product> list = new ArrayList<>();
+
+        String path = "C:\\temp\\max.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+            String line = br.readLine();
+            while (line != null) {
+                String[] fields = line.split(",");
+                list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+                line = br.readLine();
+            }
+
+            Product x = CalculationService.max(list);
+            System.out.println("Most expansive:");
+            System.out.println(x);
+        } catch (IOException e) {
+            System.out.println("Error " + e.getMessage());
+        }
+
+        scanner.close();
+    }
+}
