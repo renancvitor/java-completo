c2438ba963c03ce6dbd526c140fbd99a1861a967
Renan C Vitor <renan.vitor.cm@gmail.com>
2024-05-15 19:04:02 -0300

    DataBase mySQL - Seller entity class

--- DemonstractionDAO-JDBC/src/Application/Program.java
+++ DemonstractionDAO-JDBC/src/Application/Program.java
@@ -3,16 +3,20 @@ package Application;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Date;
 
 import DB.DB;
 import DB.DbIntegrityException;
 import Entities.Department;
+import Entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
+		System.out.println(seller);
 	}
 }
