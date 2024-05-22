daf2c985a94987869c752af2bc139347d3076b90
Renan C Vitor <renan.vitor.cm@gmail.com>
2024-05-22 15:21:06 -0300

    Project Web Service - Exception handling - update

--- webservice/src/main/java/com/rcv/webservice/services/UserService.java
+++ webservice/src/main/java/com/rcv/webservice/services/UserService.java
@@ -4,6 +4,7 @@ import com.rcv.webservice.entities.User;
 import com.rcv.webservice.repositories.UserRepository;
 import com.rcv.webservice.services.exceptions.DatabaseException;
 import com.rcv.webservice.services.exceptions.ResouceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -42,9 +43,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResouceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
