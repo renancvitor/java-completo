b50542f39fb35c8493dbb0325665bababf814df2
Renan C Vitor <renan.vitor.cm@gmail.com>
2024-05-22 06:43:21 -0300

    Project Web Service - Product-OrderItem one-to-many association

--- webservice/src/main/java/com/rcv/webservice/entities/Product.java
+++ webservice/src/main/java/com/rcv/webservice/entities/Product.java
@@ -1,8 +1,8 @@
 package com.rcv.webservice.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
-import java.io.Serial;
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Objects;
@@ -26,6 +26,9 @@ public class Product implements Serializable {
     @ManyToMany
     @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product() {
     }
@@ -80,6 +83,15 @@ public class Product implements Serializable {
 
     public void setImgUrl(String imgUrl) {
         this.imgUrl = imgUrl;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+        for (OrderItem x : items) {
+            set.add(x.getOrder());
+        }
+        return set;
     }
 
     @Override
