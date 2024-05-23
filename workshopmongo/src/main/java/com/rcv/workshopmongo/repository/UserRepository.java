9cf63e71f6d4da84b681898fc8eb56bc95add185
Renan C Vitor <renan.vitor.cm@gmail.com>
2024-05-23 09:03:29 -0300

    Project API Restful Spring Boot - MongoDB connection with repositories and services

---
+++ workshopmongo/src/main/java/com/rcv/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.rcv.workshopmongo.repository;
+
+
+import com.rcv.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+
+}
---
+++ workshopmongo/src/main/java/com/rcv/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.rcv.workshopmongo.services;
+
+
+import com.rcv.workshopmongo.domain.User;
+import com.rcv.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll() {
+        return repo.findAll();
+    }
+}
