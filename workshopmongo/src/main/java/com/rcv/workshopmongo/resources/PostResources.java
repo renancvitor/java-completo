6553b7856d529bb090b495fe6b5631233dd634c4
Renan C Vitor <renan.vitor.cm@gmail.com>
2024-05-26 13:32:49 -0300

    Project API Restful Spring Boot - Post by id

---
+++ workshopmongo/src/main/java/com/rcv/workshopmongo/resources/PostResources.java
@@ -0,0 +1,36 @@
+package com.rcv.workshopmongo.resources;
+
+import com.rcv.workshopmongo.domain.Post;
+import com.rcv.workshopmongo.domain.User;
+import com.rcv.workshopmongo.dto.UserDTO;
+import com.rcv.workshopmongo.services.PostService;
+import com.rcv.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResources {
+
+    @Autowired
+    private PostService service;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+//    @GetMapping(value = "/{codPost}")
+//    public ResponseEntity<Post> findById(@PathVariable String codPost) {
+//        Post post = postService.findById(codPost);
+//
+//        return ResponseEntity.ok().body(post);
+}
---
+++ workshopmongo/src/main/java/com/rcv/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.rcv.workshopmongo.services;
+
+
+import com.rcv.workshopmongo.domain.Post;
+import com.rcv.workshopmongo.domain.User;
+import com.rcv.workshopmongo.dto.UserDTO;
+import com.rcv.workshopmongo.repository.PostRepository;
+import com.rcv.workshopmongo.repository.UserRepository;
+import com.rcv.workshopmongo.services.exception.ObjectNotFoundExcetion;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        return repo.findById(id).orElseThrow(() -> new ObjectNotFoundExcetion("User not found."));
+    }
+}
